4f7668a22689c11d68e6f272759c22d97d63b35b
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-11 11:13:58 +0545

    string formatting

--- string_replacement_fields.py
+++ string_replacement_fields.py
@@ -3,3 +3,11 @@ print("My age is " + str(age) + "year")
 print("My age is {0} year".format(age))
 print("There are {0} days in {1},{2},{3},{4},{5},{6},{7}".format(31,"Jan","Mar","May","Jul","Aug","Oct","Dec"))
 print("Jan: {2}, Feb:{0},Mar:{2}, Apr:{1}".format(28,30,31))
+#string formatting
+for i in range(1,15):
+    print(" {0:2}'s square is {1:6} and cube is {2:6}".format(i,i**2,i**3))
+
+print()
+for i in range(1,15):
+    print(" {0:2}'s square is {1:<6} and cube is {2:^6}".format(i,i**2,i**3))
+
