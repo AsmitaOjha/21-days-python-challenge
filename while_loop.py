275b7ea18faded505c698fd9682af0ee96f4b034
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-19 20:26:07 +0545

    5th day of dashain vacation 2080

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# i=0
+# while i<10:
+#     print("i is now {}".format(i))
+#     i+=1
+
+available_exits = ['north','south', 'east','west']
+chosen_exit = ''
+while chosen_exit not in available_exits:
+    chosen_exit= input('please choose a direction:').casefold()
+    if chosen_exit.casefold()=="quit":
+        print("Game over")
+        break
+print("Aren't you glad you got out of there")
