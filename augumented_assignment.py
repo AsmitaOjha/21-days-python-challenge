46a24b59f0724bddf3ec0af9f0d8362c5be53cbc
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-21 20:30:12 +0545

    7th day of Dashain 2080

---
+++ augumented_assignment.py
@@ -0,0 +1,20 @@
+greeting = "Good"
+greeting += " Morning "
+print(greeting)
+greeting *= 5
+print(greeting)
+
+#Write Python code to perform the repeated addition shown
+#  above.
+
+#Use a for loop, and augmented assignment, to give answer 
+# the result of multiplying number by multiplier
+number = 5
+multiplier = 8
+answer = 0
+ 
+# add your loop after this comment
+for i in range(multiplier):
+    answer += number
+ 
+print(answer)
