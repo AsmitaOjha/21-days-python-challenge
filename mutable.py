e787843574e36a91a724541c272e1288785aaef4
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-27 21:01:12 +0545

    14th day of Dashain 2080

--- mutable.py
+++ mutable.py
@@ -6,3 +6,10 @@ print(id(another_list))
 shopping_list +=['cookies']
 print(shopping_list)
 print(id(shopping_list)) #list are mutable 
+a=b=c=d=e=f=another_list
+print(id(a))
+print(id(b))
+print("Adding cream")
+b.append('cream')
+print(c)
+print(d)
