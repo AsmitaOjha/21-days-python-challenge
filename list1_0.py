02997ec9408536731cfe6168838fecd31ada5192
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-27 20:33:46 +0545

    Mutable and immutable objects , 13th day of dashain

--- list1_0.py
+++ list1_0.py
@@ -1,6 +1,20 @@
 #list are used to store multiple items in a single variable
 #list are one of 4 built in data types
-fruits = ['apple', 'mango','banana','pomegranate']
-print(fruits)
-print(len(fruits))
-print(type(fruits))
+# fruits = ['apple', 'mango','banana','pomegranate']
+# print(fruits)
+# print(len(fruits))
+# print(type(fruits))
+
+
+#Immutable objects
+#Immutable object meant's it can't be changed, without being destroyed or re-createed.
+# result=True
+# another_result = result
+# print(id(result))
+# print(id(another_result))
+# result= False
+# print(id(result))
+a=5
+print(id(a))
+a+=6
+print(id(a))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-guess = int(input())
+fruits = ['apple', 'mango','banana','pomegranate']
+# print(fruits)
+# print(len(fruits))
+# print(type(fruits))
