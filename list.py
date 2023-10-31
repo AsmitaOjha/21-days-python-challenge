51695ca027753377747973165c40fccd28c58831
Asmita Ojha <asmitaojha05@gmail.com>
2023-10-31 21:26:43 +0545

    18th day of python

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
 name_list = ["Asmita","Aastha","Kapil","Prakash","Samjhana","Puja","Bhuwan","Annu","Kabir"]
 for index,name in enumerate(name_list):
     print(f"{index+1} {name}")
+family = ["me","sister","brother","father","mother"]
