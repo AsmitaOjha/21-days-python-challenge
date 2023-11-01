9957b62c1da26075901a95b22adb322c49db5599
Asmita Ojha <asmitaojha05@gmail.com>
2023-11-01 17:27:07 +0545

    18th day of dashain vacation

---
+++ extend_function.py
@@ -0,0 +1,6 @@
+fruits = ["Mango","Apple","Cherry"]
+cars = ['Ford','BMW','Volvo']
+fruits.extend(cars)
+print(fruits)
+fruits.sort()
+print(fruits)
--- removing_items_from_list.py
+++ removing_items_from_list.py
@@ -1,3 +1,8 @@
 thislist = ["apple", "banana", "cherry"]
 thislist.remove("banana")
 print(thislist)
+thislist.pop()
+#pop() method removes the specified index.
+print(thislist)
+thislist.append("pomegranate")
+print(thislist)
