301928014fd8345f4334c467b5c4ba6815e31892
Asmita Ojha <asmitaojha05@gmail.com>
2023-11-02 10:14:37 +0545

    19th day of dashain vacation

---
+++ sorted.py
@@ -0,0 +1,9 @@
+pangram = "The quick brown fox jumps over the lazy dog"
+letters= sorted(pangram)
+print(letters)
+numbers= [2.3,4.3,8.9,3.4,1.3]
+sorted_number = sorted(numbers)
+print(sorted_number)
+print(numbers)
+numbers.sort()
+print(numbers)
