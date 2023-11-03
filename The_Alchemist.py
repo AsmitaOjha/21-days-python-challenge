1598cb1290a3fa624b54ebf68d68563a0fec136f
Asmita Ojha <asmitaojha05@gmail.com>
2023-11-03 20:25:10 +0545

    The Alchemist by Paulo Coelho 20th day

--- The_Alchemist.py
+++ The_Alchemist.py
@@ -1,4 +1,12 @@
-print("The_Alchemist")
+print("The_Alchemist by Paulo Coelho")
 print('''"The wind began to pick up. He knew that wind: people called it the levanter, because on it the Moors had come from
       Levant at the eartern end of the Mediteerranean.
-      The levanter increased in intensity. Here I am, between my flock and my treasure, the boy thought."''')
+      The levanter increased in intensity. Here I am, between my flock and my treasure, the boy thought.
+      He had to choose between something he had become accustomed to and something he wanted to have. 
+      There was also the merchant's daughter, but she wasn't as important as his flock. Because she didn't depend
+      on him. Maybe she didn't even remember him. He was sure that it made no difference to her on which day
+      he appeared: for her, every day was the same, and when each day is the same as the next, it's because
+      people fail to recognize the good things that happen in their lives every day that sun rises.
+       I left my father, my mother, and the town castle behind. The have gotten used to my being away, and so have I. 
+      The sheep will get used to my not being there, too, the boy thought.
+      "''')
