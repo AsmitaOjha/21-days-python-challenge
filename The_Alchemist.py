52b3af7994d62e63a7407e80282ac0db89efea69
Asmita Ojha <asmitaojha05@gmail.com>
2023-11-03 20:14:14 +0545

    The Alchemist, 20th day of dashain vacation

---
+++ The_Alchemist.py
@@ -0,0 +1,4 @@
+print("The_Alchemist")
+print('''"The wind began to pick up. He knew that wind: people called it the levanter, because on it the Moors had come from
+      Levant at the eartern end of the Mediteerranean.
+      The levanter increased in intensity. Here I am, between my flock and my treasure, the boy thought."''')
